12232c458d8a570f84cfe9987de2c13147c8acc7
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-03-03 17:56:44 +0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/xryar/app-say-hello
+
+go 1.22.3
+
+require github.com/xryar/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/xryar/go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
